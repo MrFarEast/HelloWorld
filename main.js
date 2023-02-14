1123ddb73a038f3b832fd591eb4a7a259f561537
MrFarEast <119421922+MrFarEast@users.noreply.github.com>
2023-02-15 02:26:33 +0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log(1);
-
-const button = document.querySelector('button');
-const output = document.querySelector('.output');
-
-button.addEventListener('click', function() {
-    output.innerHTML = 'This kitten is telling you to believe in yourself!';
-});
